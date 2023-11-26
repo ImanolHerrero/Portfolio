25d8de270f650255f8bbabce1f116b5f7277dbf1
Imanol <imano.arias@gmail.com>
2023-11-26 02:40:29 -0300

    refactorizacion

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,20 +4,19 @@ import Link from 'next/link';
 
 function Navbar() {
     return (
-        <div className="flex flex-row p-5 mx-16 text-gray-800 sticky top-0 z-10">
+        <div className="flex flex-row p-5 text-gray-800 sticky top-0 z-10 bg-white w-full  shadow-sm">
             <div className="flex-1 text-3xl font-bold flex items-center">
-
-                <h1 className='font-bold'>Imanol Herrero</h1>
+                <h1 className='font-bold ml-24'>Imanol Herrero</h1>
             </div>
-            <div className="flex-1 justify-end flex items-center">
+            <div className="flex-1 justify-end flex items-center mr-24">
                 <Link href="#about">
                     <Button variant="link" className='mx-4'>About me</Button>
                 </Link>
 
                 <Button variant="link" className='mx-4'>My Projects</Button>
                 <Link href="mailto:imano.arias@gmail.com">
-                    <Button className='mx-4 bg-gray-800 font-semibold'>
-                        Let&apos;s work together!
+                    <Button className='mx-4 bg-gray-800 font-bold'>
+                        Let&apos;s work together
                     </Button>
                 </Link>
 
--- app/page.tsx
+++ app/page.tsx
@@ -10,50 +10,47 @@ export default function Home() {
   return (
     <main>
 
-      <div className="relative flex flex-col min-h-screen items-center justify-center bg-white  transition-bg">
-        <div className="absolute inset-0 overflow-hidden">
-          <div className="jumbo absolute -inset-[10px] opacity-50 ">
+      <div className="flex flex-col min-h-screen items-center justify-center bg-[url('/pattern.svg')] bg-repeat bg-cover">
+        <Navbar />
+        <div className="h-screen flex items-center justify-center">
+
+          <div className="text-center bg-white rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 p-16 shadow-lg mb-32 min-w-full">
+
+            <h1 className="text-6xl font-bold text-gray-800">
+              FullStack Web Developer
+            </h1>
+            <h1 className="mt-6  text-3xl text-gray-800">
+              Let&apos;s create great and beautiful websites together
+            </h1>
+            <h1 className="mt-1 text-3xl text-gray-800 ">
+              I want to deliver unique experiences for you
+            </h1>
+            <div className="relative mt-8 flex-row flex">
+              <Link href="https://www.linkedin.com/in/imanol-herrero-arias-932956247/">
+                <Button className="mx-2 text-md" variant="link">
+                  <Linkedin className="mr-2 h-5 w-5" />Linkedin
+                </Button>
+              </Link>
+              <Link href="https://github.com/ImanolHerrero">
+                <Button className="mx-2" variant="link">
+                  <Github className="mr-2 h-4 w-4" />Github
+                </Button>
+              </Link>
+              <Link href="mailto:imano.arias@gmail.com">
+                <Button className="mx-2" variant="link">
+                  <Send className="mr-2 h-4 w-4" />Email
+                </Button>
+              </Link>
+            </div>
           </div>
-          <Navbar />
+
         </div>
 
-        <div className="text-center mx-4 mt-40">
-          <h1 className="text-6xl font-bold text-gray-800 relative mt-20">
-            <span className="underline underline-offset-8 decoration-sky-500/30">FullStack</span> Web Developer
-          </h1>
-          <h1 className="mt-6  text-3xl text-gray-800 font-semibold">
-            Let&apos;s create great and beautiful websites together!
-          </h1>
-          <h1 className="mt-1 text-3xl text-gray-800 font-semibold">
-            I want to deliver unique experiences for you
-          </h1>
-        </div>
-
-        <div className="relative mt-8 flex-row flex">
-          <Link href="https://www.linkedin.com/in/imanol-herrero-arias-932956247/"> <Button className="mx-2" variant="link">
-            <Linkedin className="mr-2 h-4 w-4" />Linkedin
-          </Button>
-          </Link>
-          <Link href="https://github.com/ImanolHerrero">
-            <Button className="mx-2" variant="link">
-              <Github className="mr-2 h-4 w-4" />Github
-            </Button>
-          </Link>
-          <Link href="mailto:imano.arias@gmail.com">
-            <Button className="mx-2" variant="link">
-              <Send className="mr-2 h-4 w-4" />Email
-            </Button>
-          </Link>
-        </div>
-        <div className="mt-20 mb-40 animate-bounce relative">
-          <MoveDown />
-        </div>
-        <div className="mt-40 relative">
+        <div className="mt-40">
           <About />
           <Projects />
         </div>
       </div>
-
     </main>
   )
 }
